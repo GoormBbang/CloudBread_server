9cb107011aad5280e439debe17bbccba48ebc875
은지 <ijnuegnaj@gmail.com>
2025-10-09 17:46:57 +0900

    successstatus 중복 선언 삭제

--- src/main/java/com/cloudbread/global/common/code/status/SuccessStatus.java
+++ src/main/java/com/cloudbread/global/common/code/status/SuccessStatus.java
@@ -49,6 +49,7 @@ public enum SuccessStatus implements BaseCode {
     //nutrition
     NUTRITION_STATS_SUCCESS(HttpStatus.OK, "NUTRITION_STATS_SUCCESS", "영양 분석이 성공적으로 조회되었습니다."),
     NUTRITION_SUMMARY_SUCCESS(HttpStatus.OK, "NUTRITION_SUMMARY_SUCCESS", "영양 요약 성공."),
+    //NUTRITION_SUMMARY_SUCCESS(HttpStatus.OK, "NUTRITION_SUMMARY_SUCCESS", "영양 "),
 
     //calender
     CALENDAR_GET_SUCCESS(HttpStatus.OK, "CALENDAR_MONTH_SUCCESS", "조회 가능한 식단이 있습니다."),
